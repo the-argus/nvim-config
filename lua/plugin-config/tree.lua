033a358c0e4c59c3b4cd2c61d7f1e69ed48d489a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-06-04 17:28:49 -0800

    un-updated nvim-tree-icons This reverts commit ddedc3cb9d37195aac6190d886c9d73425a4c1ea.

--- lua/plugin-config/tree.lua
+++ lua/plugin-config/tree.lua
@@ -1,5 +1,26 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -13,26 +34,6 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
-  nvim_tree_icons = {
-      default = "",
-      symlink = "",
-      git = {
-        unstaged = "",
-        staged = "S",
-        unmerged = "",
-        renamed = "➜",
-        deleted = "",
-        untracked = "U",
-        ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
