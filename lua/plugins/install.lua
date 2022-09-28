f0c0175db6ac00ffbbcc61b3197e7ae9b65b518e
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-28 00:58:47 -0400

    no change, commented

--- lua/plugins/install.lua
+++ lua/plugins/install.lua
@@ -148,6 +148,8 @@ return packer.startup(function(use)
         end
     }
 
+    -- use { 'nvim-telescope/telescope-fzf-native.nvim',
+    --     run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     use {
         'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' }, { 'kyazdani42/nvim-web-devicons' } }
