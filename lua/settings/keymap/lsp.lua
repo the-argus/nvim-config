de0bcf50576cd813e2712286e932b807a94a9f43
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-03 13:42:16 -0400

    add Fix command for accepting changes

--- lua/settings/keymap/lsp.lua
+++ lua/settings/keymap/lsp.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-M.create_keymaps = function (bufnr)
+M.create_keymaps = function(bufnr)
     local opts = { noremap = true, silent = true }
+    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+    vim.cmd [[ command! Fix execute 'lua vim.lsp.buf.code_action()' ]]
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gk", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
@@ -21,7 +23,6 @@ M.create_keymaps = function (bufnr)
     ) ]]
     vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
 return M
