00544eecf942da828717f694225739261d1a3177
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-09 13:50:35 -0500

    fix minor deprecations

--- modules/lsp.nix
+++ modules/lsp.nix
@@ -48,12 +48,12 @@ with dsl; {
       cmd = ["${pkgs.rnix-lsp}/bin/rnix-lsp"];
       capabilities =
         rawLua
-        "require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())";
+        "require('cmp_nvim_lsp').default_capabilities()";
     };
 
     quick_lint_js.setup = callWith {
       cmd = [
-        "${quick-lint-js}/bin/quick-lint-js"
+        "${pkgs.quick-lint-js}/bin/quick-lint-js"
       ];
     };
 
