1c593e8242b7934dca98ef0157d3996f48e50e2f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-15 12:20:37 -0500

    configure html ls, also fix lua ls config

---
+++ lua/plugin-config/lsp/settings/html.lua
@@ -0,0 +1,14 @@
+return {
+    cmd = { "html-languageserver", "--stdio" },
+    filetypes = { "html" },
+    {
+        configurationSection = { "html", "css", "javascript" },
+        embeddedLanguages = {
+            css = true,
+            javascript = true
+        },
+        provideFormatter = true
+    },
+    settings = {},
+    single_file_support = true
+}
