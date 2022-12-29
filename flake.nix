91214df6efa8e830506da411eab85803fbdc9b17
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-29 11:58:11 -0600

    update inputs and add unstable for yuck-vim

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs?rev=e12211201092f08c24d710c1697cca16afae3a4c;
     nixpkgs-old.url = github:NixOS/nixpkgs?ref=nixos-22.05;
+    nixpkgs-unstable.url = github:NixOS/nixpkgs?ref=nixos-unstable;
     nix2vim.url = github:gytis-ivaskevicius/nix2vim;
     nix2vim.inputs.nixpkgs.follows = "nixpkgs";
     banner = {
@@ -16,6 +17,7 @@
     self,
     nixpkgs,
     nixpkgs-old,
+    nixpkgs-unstable,
     nix2vim,
     banner,
   }: let
@@ -65,6 +67,7 @@
     # pkgs-old = genSystems (system: mkPkgs system nixpkgs-old);
 
     pkgs = genSystems (system: mkPkgs system nixpkgs-old);
+    unstable = genSystems (system: mkPkgs system nixpkgs-unstable);
   in {
     packages = genSystems (system: let
       mkBuilderInputs = {bannerPalette, ...}: {
@@ -197,6 +200,8 @@
 
           # helps nesting to look better
           indent-blankline-nvim
+
+          unstable.${system}.vimPlugins.yuck-vim
         ];
 
         lua =
