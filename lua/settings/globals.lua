a33612552b57385501b0bbae3f3882a44df3f202
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-11 13:56:21 -0600

    remove breaking filetype global :/

--- lua/settings/globals.lua
+++ lua/settings/globals.lua
@@ -1,10 +1,6 @@
 local g = vim.g
 g.mapleader = " " -- mod key for mappings
 
--- misc stuff that keymaster used, idk if im gonna use it
-g.do_filetype_lua = 1 -- for now, Lua filetype detection is opt-in
-g.did_load_filetypes = 0 -- if this variable exists, $VIMRUNTIME/filetype.vim will not run
-
 -- neovide
 g.neovide_cursor_animation_length = 0.13
 g.neovide_cursor_trail_length = 0.8
