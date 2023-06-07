b12e6ddba6df4a825725b970d5224bc128461e2f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-06-07 09:27:08 -0400

    add rust-analyzer settings

---
+++ lua/plugin-config/lsp/settings/rust_analyzer.lua
@@ -0,0 +1,12 @@
+return {
+    cmd       = "rust-analyzer",
+    filetypes = { "rust" },
+    settings  = {
+        ["rust-analyzer"] = {
+            diagnostics = { experimental = { enable = true } },
+            check = {
+                command = "clippy"
+            }
+        }
+    }
+}
