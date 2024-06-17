94a4697adadce128b906713d16569a746db91af3
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-06-17 00:08:27 -0400

    actually include new keybinds

--- lua/settings/keymap/init.lua
+++ lua/settings/keymap/init.lua
@@ -2,6 +2,7 @@ require "settings.keymap.buffers"
 require "settings.keymap.editing"
 require "settings.keymap.window"
 require "settings.keymap.tree"
+require "settings.keymap.debugging"
 local dvorak_utils = require("settings.keymap.dvorak-overrides")
 if UsingDvorak then
     dvorak_utils.enable_dvorak()
