7c92058165b90a7d77e4ae9f52e8e5fd6dcb1977
Ian McFarlane <i.mcfarlane2002@gmail.com>
2026-09-04 10:47:01 -0400

    configure leap a bit

--- lua/perpluginconfig/leap.lua
+++ lua/perpluginconfig/leap.lua
@@ -3,21 +3,26 @@ if not present then
     return
 end
 
-vim.keymap.set({'n', 'x', 'o'}, 'z', '<Plug>(leap)')
-vim.keymap.set('n',             'Z', '<Plug>(leap-from-window)')
-vim.keymap.set('n',        'z', '<Plug>(leap-anywhere)')
+vim.keymap.set({ 'n', 'x', 'o' }, 'z', '<Plug>(leap)')
+vim.keymap.set('n', 'Z', '<Plug>(leap-from-window)')
+vim.keymap.set('n', 'z', '<Plug>(leap-anywhere)')
 -- vim.keymap.set({'x', 'o'}, 'z', '<Plug>(leap)')
 
 -- Exclude whitespace and the middle of alphabetic words from preview:
 --   foobar[baaz] = quux
 --   ^----^^^--^^-^-^--^
 leap.opts.preview_filter =
-  function (ch0, ch1, ch2)
-    return not (
-      ch1:match('%s') or
-      ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
-    )
-  end
+    function(ch0, ch1, ch2)
+        return not (
+            ch1:match('%s') or
+            ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
+        )
+    end
 
 leap.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
-require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
+local leap_user_ok, leap_user = pcall(require, 'leap.user')
+if not leap_user_ok then
+    return
+end
+leap_user.set_repeat_keys('<enter>', '<backspace>')
